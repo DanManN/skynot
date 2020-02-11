f40abe95fab01467b45e96445a66eda11b801d5d
Daniel Nakhimovich <dnahimov@gmail.com>
2020-02-11 05:27:56 -0500

    first

---
+++ Assets/Scripts/Util.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Util
+{
+    public static Vector3 forward()
+    {
+        return Vector3.Normalize(Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up));
+    }
+
+    public static Vector3 backward()
+    {
+        return -forward();
+    }
+
+    public static Vector3 right()
+    {
+        return Vector3.Normalize(Vector3.ProjectOnPlane(Camera.main.transform.right, Vector3.up));
+    }
+
+    public static Vector3 left()
+    {
+        return -right();
+    }
+}
