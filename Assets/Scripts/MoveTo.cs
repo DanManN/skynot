54f3ca58127eb26a46c57f089b22073a8310fd44
ashdunn <31973458+ashdunn@users.noreply.github.com>
2020-02-17 18:10:15 -0500

    base scene and agents

---
+++ Assets/Scripts/MoveTo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTo : MonoBehaviour
+{
+
+    public Transform goal;
+    private UnityEngine.AI.NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            Physics.Raycast(ray, out hit, 100);
+
+            agent.destination = hit.point;
+            agent.isStopped = false;
+        }
+    }
+}
